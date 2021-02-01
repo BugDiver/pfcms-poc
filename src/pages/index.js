2babebb36e6dc6c609b00dc7035e3d361cfa5d7d
BugDiver <vinaysh@thoughtworks.com>
2021-02-01 17:34:02 +0530

    Initial commit.
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Library</h1>
+      <h2>A simple Library with little info about books</h2>
+    </div>
+  )
+}
