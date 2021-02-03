72ea2e092cf54dbfe010293978f63bd76cf8216f
Neha Sanserwal <neha16sanserwal@gmail.com>
2021-02-03 17:34:55 +0530

    Revert "added test suit jest"
    This reverts commit 4c68ed8a8ef9cd23d6a10d5fbbef77f1183686b2.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,26 +1,27 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import headerStyle from "./header.module.scss"
-export const query = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
+
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
       }
     }
-  }
-`)
-const Header = props => {
-  const { title, description } = props.data.site.siteMetadata
+  `)
+
   return (
     <header className={headerStyle.header}>
       <h1 className={"f1 " + headerStyle.h1}>
         <Link className={headerStyle.link} to="/">
-          {title}
+          {data.site.siteMetadata.title}
         </Link>
       </h1>
-      <p>{description}</p>
+      <p>{data.site.siteMetadata.description}</p>
     </header>
   )
 }
