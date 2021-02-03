69386175c4769e8e5d1d63a34cd177a09a77ac56
Neha Sanserwal <neha16sanserwal@gmail.com>
2021-02-03 14:19:19 +0530

    styled book template

--- src/templates/book.js
+++ src/templates/book.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-
+import bookStyles from "./book.module.scss"
 export const query = graphql`
   query($slug: String!) {
     contentfulBook(slug: { eq: $slug }) {
       title
       author
+      cover {
+        file {
+          url
+        }
+      }
       summary {
         summary
       }
@@ -16,16 +21,23 @@ export const query = graphql`
 
 const Book = props => {
   const book = props.data.contentfulBook
-  const style = {
-    marginLeft: "20%",
-    marginRight: "20%",
-    marginTop: "50px",
-  }
+
   return (
     <Layout>
-      <h2>{book.title}</h2>
-      <span>{book.author.join(", ")}</span>
-      <p style={style}>{book.summary.summary}</p>
+      <div className={bookStyles.container}>
+        <div className={bookStyles.cover}>
+          <img src={book.cover.file.url} alt={book.title} />
+        </div>
+        <div className={bookStyles.content}>
+          <div className={bookStyles.heading}>
+            <h2>{book.title}</h2>
+            <span>{book.author.join(", ")}</span>
+          </div>
+          <div className={bookStyles.description}>
+            <p>{book.summary.summary}</p>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
